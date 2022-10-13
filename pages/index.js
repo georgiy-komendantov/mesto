01cd5d24652d3ac8ccc1fa505cdcb12ca728d507
georgiy-komendantov <georgiykomendantov@gmail.com>
2022-10-13 10:27:56 +0300

    fix 2

--- pages/index.js
+++ pages/index.js
@@ -4,44 +4,33 @@ let about = 'Исследователь океана';
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileText = document.querySelector('.profile__text');
 const profileAbout = document.querySelector('.profile__about');
+const popupWrapper = document.querySelector('.popup__wraper');
+
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputAbout = document.querySelector('.popup__input_type_about');
 
 profileText.innerHTML = name;
 profileAbout.innerHTML = about;
 
-const popups = document.querySelector('.popups');
 
-function onProfileClick() {
-    popups.innerHTML = `
-    <div class="popup__wraper">
-        <form class="popup">
-            <h2 class="popup__title">Редактировать профиль</h2>
+function exitForm(event){
+    event?.preventDefault();
+    popupWrapper.style.visibility= "hidden";
+}
 
-            <div class="popup_container">
-                <input class="popup__input popup__input_type_name">
-                <span class="popup__error"></span>
-                <input class="popup__input popup__input_type_about">
-                <span class="popup__error"></span>
-            </div>
-
-            <button class="popup__button-save">Сохранить</button>
-            <button class="popup__button-exit"></button>
-        </form>
-    </div>
-    `;
+function onProfileClick(e) {
+    e.preventDefault();
+    popupWrapper.style.visibility= "visible";
     const popupButtonExit = document.querySelector('.popup__button-exit');
 
-    function exitForm(){
-        popups.innerHTML = ``;
-    }
     popupButtonExit.addEventListener('click', exitForm);
 
-    const popupInputName = document.querySelector('.popup__input_type_name');
-    const popupInputAbout = document.querySelector('.popup__input_type_about');
     popupInputName.value = name;
     popupInputAbout.value = about;
 
     const formElement = document.querySelector('.popup');
-    function saveForm(){
+    function saveForm(event){
+        event.preventDefault();
         name = popupInputName.value;
         about = popupInputAbout.value;
         profileText.innerHTML = name;
