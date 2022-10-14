7f4c096022192689bd185843c897d9532e136634
georgiy-komendantov <georgiykomendantov@gmail.com>
2022-10-14 19:29:59 +0300

    review fixes

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,40 @@
-let name = 'Жак-Ив Кусто';
-let about = 'Исследователь океана';
-
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileText = document.querySelector('.profile__text');
 const profileAbout = document.querySelector('.profile__about');
-const popupWrapper = document.querySelector('.popups__wrapper');
+const popupWrapper = document.querySelector('.popups');
 
 const popupInputName = document.querySelector('.popup__input_type_name');
 const popupInputAbout = document.querySelector('.popup__input_type_about');
 
-profileText.innerHTML = name;
-profileAbout.innerHTML = about;
+const popupButtonExit = document.querySelector('.popup__button-exit');
+const formElement = document.querySelector('.popup');
 
+const formTitle = document.querySelector('.popup__title');
 
-function exitForm(event){
-    event?.preventDefault();
-    popupWrapper.style.visibility= "hidden";
+function exitForm(e){
+    if(e) {
+        e.preventDefault();
+    }
+    popupWrapper.classList.add('popups_disabled');
+}
+
+function saveForm(e){
+    e.preventDefault();
+    profileText.textContent = popupInputName.value;
+    profileAbout.textContent = popupInputAbout.value;
+    formTitle.textContent = '';
+    exitForm();
 }
 
 function onProfileClick(e) {
     e.preventDefault();
-    popupWrapper.style.visibility= "visible";
-    const popupButtonExit = document.querySelector('.popup__button-exit');
 
-    popupButtonExit.addEventListener('click', exitForm);
-
-    popupInputName.value = name;
-    popupInputAbout.value = about;
-
-    const formElement = document.querySelector('.popup');
-    function saveForm(event){
-        event.preventDefault();
-        name = popupInputName.value;
-        about = popupInputAbout.value;
-        profileText.innerHTML = name;
-        profileAbout.innerHTML = about;
-        exitForm();
-    }
-    formElement.addEventListener('submit', saveForm)
+    popupWrapper.classList.remove('popups_disabled');
+    popupInputName.value = profileText.textContent;
+    popupInputAbout.value = profileAbout.textContent;
+    formTitle.textContent = 'Редактировать профиль';
 }
 
-profileEditButton.addEventListener('click', onProfileClick)
+profileEditButton.addEventListener('click', onProfileClick);
+popupButtonExit.addEventListener('click', exitForm);
+formElement.addEventListener('submit', saveForm);
