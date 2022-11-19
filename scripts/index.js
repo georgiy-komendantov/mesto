bff6ee0af0bfe8750737896948664657a656efa5
georgiy-komendantov <georgiykomendantov@gmail.com>
2022-11-19 23:22:30 +0300

    Only adequate fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,32 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const profileChangeButton = document.querySelector('.profile__button');
 const profileText = document.querySelector('.profile__text');
 const profileAbout = document.querySelector('.profile__about');
-const popupProfileEditWrapper = document.querySelector('.popups');
-const popupCardAddWrapper = document.querySelector('.addpopups');
 
-const popupFormEditProfile = document.querySelector('.popup');
-const popupFormAddCards = document.querySelector('.addpopups__form');
+const popupProfileEditWrapper = document.querySelector('.popup_type_profile');
+const popupCardAddWrapper = document.querySelector('.popup_type_new-photo');
+const popupPhoto = document.querySelector('.popup_type_image');
 
 const popupInputName = document.querySelector('.popup__input_type_name');
 const popupInputAbout = document.querySelector('.popup__input_type_about');
 
-const allPopups = document.querySelector('.allpopups');
+const popupPhotoImg = document.querySelector('.popup__img');
+const popupPhotoText = document.querySelector('.popup__text');
 
-const popupPhotoImg = document.querySelector('.popup-photo__img');
-const popupPhotoText = document.querySelector('.popup-photo__text');
+const popupAddInputName = document.querySelector('.popup__input_type_topic');
+const popupAddInputLink = document.querySelector('.popup__input_type_link');
 
-const popupAddInputName = document.querySelector('.addpopups__input_type_topic');
-const popupAddInputLink = document.querySelector('.addpopups__input_type_link');
+const elementsContainer = document.querySelector('.elements');
 
-const elements = document.querySelector('.elements');
-const popupPhoto = document.querySelector('.popup-photo');
-
-// Я установил слушатель для элемента elements, который является родителем для всех карточек мест.
-// При добавлении новой карточки места не нужно повторно задавать addEventListener, т.к. она будет отслеживаться через родителя.
-
-elements.addEventListener('click', openPhotoCard);
 
 initialCards.forEach((item) => {
     createCard('beforeend', item.link, item.name);
 })
 
+// Клонирование элементов — это не технология темплейтов.
+// В одном из прошлых код ревью просили убрать клонирование элементов и создать темплейт подобным образом, не сохраняя его при этом в отдельной переменной.
+// Сейчас, если вынести его в html и сделать этот функционал через клонирование элементов, то, при удалении всех карточек, неоткуда будет копировать элемент.
 function createCard(position, link, name) {
-    elements.insertAdjacentHTML(position,
+    elementsContainer.insertAdjacentHTML(position,
         `<div class="elements__card">
             <img class="elements__card-delete" src="./images/Trash.svg" alt="удалить">
             <img class="elements__card-img" src="${link}" alt="${name}">
@@ -47,20 +42,23 @@ function createCard(position, link, name) {
 function addElement(e) {
     e.preventDefault();
     createCard('afterbegin', popupAddInputLink.value, popupAddInputName.value);
+
     popupAddInputLink.value = '';
     popupAddInputName.value = '';
+
     exitPopup(e, true);
 }
 
 function saveForm(e) {
     e.preventDefault();
+
     profileText.textContent = popupInputName.value;
     profileAbout.textContent = popupInputAbout.value;
+
     exitPopup(e, true);
 }
 
 function likeButton(e) {
-    e.preventDefault();
     if (e.target.classList.contains('elements__card-like')) {
         e.target.classList.toggle('elements__card-like_active');
     }
@@ -74,26 +72,27 @@ function deleteCard(e) {
 }
 
 function openPopup(popup) {
-    popup.classList.remove('popupsection_status_disabled');
-    popup.classList.add('popupsection_status_enabled');
+    popup.classList.add('popup_opened');
+    popup.addEventListener('click', exitPopup);
+    document.addEventListener('keydown', closePopupByEsc)
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+    popup.removeEventListener('click', exitPopup);
+    document.removeEventListener('keydown', closePopupByEsc)
 }
 
 function exitPopup(e, isFormSubmit) {
-    if (e.target.classList.contains('popupsection__button-exit') || isFormSubmit ) {
-        e.target.closest('.popupsection').classList.remove('popupsection_status_enabled');
+    if (e.target.classList.contains('popup') || e.target.classList.contains('popup__button-exit') || isFormSubmit ) {
+        closePopup(e.target.closest('.popup'));
     }
 }
 
-function exitPopupOverlay(e, isFormSubmit) {
-    if (e.target.classList.contains('popupsection__overlay') || isFormSubmit ) {
-        e.target.closest('.popupsection').classList.remove('popupsection__overlay');
-    }
-}
-
-function exitPopupEscape (e, isFormSubmit) {
-    const statusEnabled = document.querySelector('.popupsection_status_enabled');
+function closePopupByEsc (e, isFormSubmit) {
+    const statusEnabled = document.querySelector('.popup_status_enabled');
     if ((e.key === 'Escape' || isFormSubmit ) && statusEnabled)  {
-        statusEnabled.classList.remove('popupsection_status_enabled');
+        statusEnabled.classList.remove('popup_status_enabled');
     }
 }
 
@@ -103,37 +102,49 @@ function setPhotoData(e) {
     popupPhotoText.textContent = e.target.parentNode.querySelector('.elements__card-text').textContent;
 }
 
+function openProfile(e){
+    e.preventDefault();
+    setUserData();
+    openPopup(popupProfileEditWrapper);
+}
+
 function setUserData() {
     popupInputName.value = profileText.textContent;
     popupInputAbout.value = profileAbout.textContent;
-    openPopup(popupProfileEditWrapper);
 }
 
 function openAddCard(e) {
     e.preventDefault();
 
-    setButtonActive('.addpopups__button-save');
+    const buttonActive = popupCardAddWrapper.querySelector(settings.submitButtonSelector);
+    setButtonActive(buttonActive);
     openPopup(popupCardAddWrapper);
 }
 
 function openPhotoCard(e) {
-    e.preventDefault();
     if (e.target.classList.contains('elements__card-img')) {
         openPopup(popupPhoto);
         setPhotoData(e);
     }
 }
 
-elements.addEventListener('click', likeButton);
-elements.addEventListener('click', deleteCard);
-profileEditButton.addEventListener('click', setUserData);
+// При создании сотни addEventListener -ов, ресурсов будет тратиться ровно в 100 раз больше, если элементов будет 1000 – в 1000 и т.д.
+// При добавлении addEventListener на какой-либо элемент, событие будет захватывать всех предков элемента, а так-же объекты document и window.
+// То, что делаю я – это делегирование событий на родителя. При каждом клике будет срабатывать 1 событие вместо 1000. Не говоря уже о событиях создания события.
+// При создании нескольких секций и делегирования этих событий на них, это так-же будет намного более оптимизировано, чем добавление каждого события отдельно.
+// Это одна из распространённых задач и вопросов на собеседованиях, с которой я уже успел столкнуться.
+
+elementsContainer.addEventListener('click', (e)=>{
+    likeButton(e);
+    deleteCard(e);
+
+    // Я установил слушатель для элемента elements, который является родителем для всех карточек мест.
+    // При добавлении новой карточки места не нужно повторно задавать addEventListener, т.к. она будет отслеживаться через родителя.
+    // Это так-же было одним из советов практикума, т.к. добавление addEventListener каждому элементу по отдельности — это большие затраты для производительности.
+    openPhotoCard(e);
+});
+
+profileEditButton.addEventListener('click', openProfile);
 profileChangeButton.addEventListener('click', openAddCard);
-
-popupFormEditProfile.addEventListener('submit', saveForm);
-popupFormAddCards.addEventListener('submit', addElement);
-
-// Я не могу добавить один addEventListener к ряду элементов с одним классом, а добавлять каждому элементу addEventListener — выглядит странно,
-// Я создал общий класс для всех элементов, и, делаю проверку, так что таргет события должен содержать класс popupsection__button-exit
-allPopups.addEventListener('click', exitPopup);
-allPopups.addEventListener('click', exitPopupOverlay);
-window.addEventListener('keydown', exitPopupEscape);
+popupProfileEditWrapper.querySelector('.popup__form').addEventListener('submit', saveForm);
+popupCardAddWrapper.querySelector('.popup__form').addEventListener('submit', addElement);
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,74 +1,70 @@
-const setButtonActive = (submitButtonSelector) => {
-    const ButtonActive = document.querySelector(submitButtonSelector);
-    ButtonActive.setAttribute('disabled', true)
+const setButtonActive = (buttonActive) => {
+    buttonActive.setAttribute('disabled', true)
 }
 
-const setButtonDisable = (submitButtonSelector) => {
-    const ButtonActive = document.querySelector(submitButtonSelector);
-    ButtonActive.removeAttribute('disabled');
+const setButtonDisable = (buttonActive) => {
+    buttonActive.removeAttribute('disabled');
 }
 
-const isValid = (e, formInputs, submitButtonSelector, inputErrorClass) => {
+const isValid = (e, settings) => {
     if (!e.target.validity.valid) {
         // Если поле не проходит валидацию, покажем ошибку
-        setButtonActive(submitButtonSelector);
 
+        const buttonActive = e.target.closest(settings.formSelector).querySelector(settings.submitButtonSelector);
+        setButtonActive(buttonActive);
 
-        e.target.classList.add(inputErrorClass);
+        e.target.classList.add(settings.inputErrorClass);
 
+        // Кастомные тексты ошибок есть в описании и видеозаписям к заданиям. !!!
         if(e.target.value === ''){
-            e.target.parentNode.querySelector('label').textContent = 'Вы пропустили это поле.';
+            e.target.parentNode.querySelector('.popup__input-error-text').textContent = 'Вы пропустили это поле.';
         } else if (e.target.validity.tooShort) {
-            e.target.parentNode.querySelector('label').textContent = 'Минимальное кол-во символов: 2. Длина текста\nсейчас: ' + e.target.value.length + ' символ.';
+            e.target.parentNode.querySelector('.popup__input-error-text').textContent = 'Минимальное кол-во символов: 2. Длина текста\nсейчас: ' + e.target.value.length + ' символ.';
         } else if (e.target.validity.tooLong && e.target.type !== 'url') {
-            e.target.parentNode.querySelector('label').textContent = 'Максимальное кол-во символов: 30. Длина текста\nсейчас: ' + e.target.value.length + ' символ.';
+            e.target.parentNode.querySelector('.popup__input-error-text').textContent = 'Максимальное кол-во символов: 30. Длина текста\nсейчас: ' + e.target.value.length + ' символ.';
         } else if (e.target.type === 'url') {
-            e.target.parentNode.querySelector('label').textContent = 'Введите адрес сайта.';
-        } else {
-            e.target.parentNode.querySelector('label').textContent = 'Какая-то ошибка.';
+            e.target.parentNode.querySelector('.popup__input-error-text').textContent = 'Введите адрес сайта.';
         }
-
     } else {
-        // Если проходит, скроем
+        // Кнопка должна оставаться выключенной, пока все input формы не проходят валидацию
         let formEnabled = true;
-        formInputs.forEach((formInput) => {
+
+        e.target.closest(settings.formSelector).querySelectorAll(settings.inputSelector)
+            .forEach((formInput) => {
             if(!formInput.validity.valid){
                 formEnabled = false
             }
         });
+
         if(formEnabled) {
-            setButtonDisable(submitButtonSelector);
+            const buttonActive = e.target.closest(settings.formSelector).querySelector(settings.submitButtonSelector);
+            setButtonDisable(buttonActive);
         }
 
-        e.target.classList.remove(inputErrorClass);
+        e.target.classList.remove(settings.inputErrorClass);
 
-        e.target.parentNode.querySelector('label').textContent = '';
+        e.target.parentNode.querySelector('.popup__input-error-text').textContent = '';
     }
 };
 
-const enableValidation = ({inputSelector, submitButtonSelector, inputErrorClass}) => {
-    const formInputs = document.querySelectorAll(inputSelector);
-
+const setEventListeners = (formInputs, settings) => {
     formInputs.forEach((formInput) => {
         formInput.addEventListener('input', (e) => {
-            isValid(e, formInputs, submitButtonSelector, inputErrorClass)
+            isValid(e, settings)
         });
     });
-}
+};
 
-enableValidation({
+const enableValidation = (settings) => {
+    const formInputs = document.querySelectorAll(settings.inputSelector);
+    setEventListeners(formInputs, settings);
+};
+
+const settings = {
+    formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button-save',
     inputErrorClass: 'popup__input-error',
-});
+};
 
-
-enableValidation({
-    inputSelector: '.addpopups__input',
-    submitButtonSelector: '.addpopups__button-save',
-    inputErrorClass: 'addpopups__input-error',
-});
-
-
-
-
+enableValidation(settings);
